50ea29f713f257ceba711e7cbae6e82c61d6c22b
David Thames <davidct@vt.edu>
2019-03-02 22:36:33 -0500

    Added sqllite model for expences lookup table

--- budgetio/budget_calc/models.py
+++ budgetio/budget_calc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ExpencesLookupTable(models.Model):
+    salary = models.IntegerField()
+    food = models.IntegerField()
+    housing = models.IntegerField()
+    transportation = models.IntegerField()
+    healthcare = models.IntegerField()
+    taxes = models.IntegerField()
+    other = models.IntegerField()
+
+
